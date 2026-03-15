bbfce89b7763204184f0270d5c730306f5d3f065
Mampers <36115147+Mampers@users.noreply.github.com>
2026-03-15 16:52:35 +0000

    Delete 03_SQL_Data_Warehouse/01_Create_Schemas.sql

--- 03_SQL_Data_Warehouse/01_Create_Schemas.sql
+++
@@ -1,5 +0,0 @@
--- 00_create_schemas.sql
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'bronze') EXEC('CREATE SCHEMA bronze');
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'silver') EXEC('CREATE SCHEMA silver');
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'gold')   EXEC('CREATE SCHEMA gold');
-GO
